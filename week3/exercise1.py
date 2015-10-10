c7fe1d6878dde52610e7d56962ac54a4dcd9b299
Dan Freedman <freedman.dan@gmail.com>
2015-10-10 00:02:15 -0700

    adding week3 exercises

---
+++ week3/exercise1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+### IMPORT REQUIRED MODULES
+
+import pysnmp
+import snmp_helper
+import email_helper
+import pickle
+import yaml
+import json
+import time
+
+### SNMP INPUTS ###
+
+snmpaddr = '50.76.53.27'		# snmp query device ip address
+snmpcomm = 'galileo'			# snmpv1/v2c community string
+v3uname = 'pysnmp'			# snmpv3 username
+v3authtype = 'sha'			# snmpv3 authentication type
+v3authkey = 'galileo1'			# snmpv3 authentication key
+v3encrtype = 'aes128'			# snmpv3 encryption type
+v3encrkey = 'galileo1'			# snmpv3 encryption key
+snmpport = 8061				# snmp port
+oid1 = '1.3.6.1.4.1.9.9.43.1.1.1.0' 	# ccmHistoryRunningLastChanged
+oid2 = '1.3.6.1.4.1.9.9.43.1.1.2.0' 	# ccmHistoryRunningLastSaved
+oid3 = '1.3.6.1.4.1.9.9.43.1.1.3.0' 	# ccmHistorystartupLastChanged
+oid4 = '.1.3.6.1.2.1.1.5.0'		# system name
+
+### EMAIL ALERT INPUTS ###
+sender = 'week3exercise1@pynet.com'
+recipient = 'freedman.dan@gmail.com'
+subject = 'CONFIGURATION CHANGE ALERT'
+message = '''
+pynet-rtr2 configuration has changed within the last 5 minutes
+'''
+
+def CheckConfig():
+	global timestamp
+	snmp_device = (snmpaddr, snmpport)
+	snmp_user = (v3uname, v3authkey, v3encrkey)
+	snmp_data = snmp_helper.snmp_get_oid_v3(snmp_device, snmp_user, oid1, v3authtype, v3encrtype)
+	output = snmp_helper.snmp_extract(snmp_data)
+	if bool(timestamp) == False:
+		timestamp.append(output)
+		print '\nWe have begun monitoring this device for configuration changes.'
+		print '%s %s' % ('The timestamp for the device is currently: ', output)
+		print 'We will check the device again in 5 minutes.\n'
+	elif timestamp[0] != '':
+		if output == timestamp[0]:
+			print 'The configuration has not changed in the last 5 minutes.'
+			print '%s %s' % ('The timestamp for the device is currnetly: ', output)
+			print 'We will check the device again in 5 minutes.\n'
+		elif output != timestamp[0]:
+			timestamp[0] = output
+			print 'The configuation has changed.'
+			print 'Sending email alert and checking device again in 5 minutes.\n'
+			email_helper.send_mail(recipient, subject, message, sender)
+
+timestamp = []
+count = 0
+while count < 60:
+        CheckConfig()
+        count += 5
+        time.sleep(300)
+
---
+++ week3/exercise2.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+### IMPORT REQUIRED MODULES
+
+import pysnmp
+import snmp_helper
+import time
+import pygal
+
+### SNMP INPUTS ###
+
+snmpaddr = '50.76.53.27'                # snmp query device ip address
+snmpcomm = 'galileo'                    # snmpv1/v2c community string
+v3uname = 'pysnmp'                      # snmpv3 username
+v3authtype = 'sha'                      # snmpv3 authentication type
+v3authkey = 'galileo1'                  # snmpv3 authentication key
+v3encrtype = 'aes128'                   # snmpv3 encryption type
+v3encrkey = 'galileo1'                  # snmpv3 encryption key
+snmpport = 8061                         # snmp port
+oid1 = '1.3.6.1.2.1.2.2.1.10.5'		# Fa4 interface in octets
+oid2 = '1.3.6.1.2.1.2.2.1.11.5'		# Fa4 interface in packets
+oid3 = '1.3.6.1.2.1.2.2.1.16.5'		# Fa4 interface out octets
+oid4 = '1.3.6.1.2.1.2.2.1.17.5'		# Fa4 interface out packets
+oid5 = '1.3.6.1.2.1.2.2.1.2.5'		# Fa4 interface description
+
+def CheckInterfaceStats(oid, value):
+        global in_octets,in_packets,out_octets,out_packets
+        snmp_device = (snmpaddr, snmpport)
+        snmp_user = (v3uname, v3authkey, v3encrkey)
+	snmp_data = snmp_helper.snmp_get_oid_v3(snmp_device, snmp_user, oid, v3authtype, v3encrtype)
+	output = snmp_helper.snmp_extract(snmp_data)
+	output = int(output)
+	if value == '1':
+		in_octets.append(output)
+	if value == '2':
+		in_packets.append(output)
+	if value == '3':
+		out_octets.append(output)
+	if value == '4':
+		out_packets.append(output)
+
+#	if bool(in_octets) == False and value == '1':
+#		in_octets.append(output)
+#	elif bool(in_packets) == False and value == '2':
+#		in_packets.append(output)
+#	elif bool(out_octets) == False and value == '3':
+#		out_octets.append(output)
+#	elif bool(out_packets) == False and value == '4':
+#		out_packets.append(output)
+#	else:
+#		if value == '1':
+#			in_octets.append(output - in_octets[len(in_octets) - 1])
+#		if value == '2':
+#			in_packets.append(output - in_packets[len(in_packets) - 1])
+#		if value == '3':
+#			out_octets.append(output - out_octets[len(out_octets) - 1])
+#		if value == '4':
+#			out_packets.append(output - out_packets[len(out_packets) - 1])
+
+def BuildGraph(number):
+	global new_in_octets,new_out_octets,new_in_packets,new_out_packets
+	line_chart = pygal.Line()
+	line_chart.x_labels = ['5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60']
+	if number == '1':
+		line_chart.title = 'Input/Output Octets'
+		line_chart.add('InOctets', new_in_octets)
+		line_chart.add('OutOctets', new_out_octets)
+		line_chart.render_to_file('octets.svg')
+	if number == '2':
+		line_chart.title = 'Input/Output Packets'
+		line_chart.add('InPackets', new_in_packets)
+		line_chart.add('OutPackets', new_out_packets)
+		line_chart.render_to_file('packets.svg')
+
+in_octets = []
+in_packets = []
+out_octets = []
+out_packets = []
+count = 0
+
+while count < 65:
+        CheckInterfaceStats(oid1, '1')
+	CheckInterfaceStats(oid2, '2')
+	CheckInterfaceStats(oid3, '3')
+	CheckInterfaceStats(oid4, '4')
+	print 'For one hour, INTERFACE STATISTICS will be updated once every 5 minutes.  Current timer: %s minutes.  Current time: %s' % (count, time.ctime().split(' ')[-2])
+	print 'In Octets: %s' % in_octets
+	print 'In Packets: %s' % in_packets
+	print 'Out Octets: %s' % out_octets
+	print 'Out Packets: %s' % out_packets
+        count += 5
+        time.sleep(300)
+
+# PREP NEWLIST
+
+new_in_octets = []
+for i,v in enumerate(in_octets):
+	if i == 0:
+ 		new_in_octets.append((v-v))
+	if i > 0:
+		new_in_octets.append((v) - (in_octets[(i - 1)]))
+
+new_in_packets = []
+for i,v in enumerate(in_packets): 
+        if i == 0:
+                new_in_packets.append((v-v))
+        if i > 0:
+                new_in_packets.append((v) - (in_packets[(i - 1)]))
+
+new_out_octets = []
+for i,v in enumerate(out_octets): 
+        if i == 0:
+                new_out_octets.append((v-v))
+        if i > 0:
+                new_out_octets.append((v) - (out_octets[(i - 1)]))
+
+new_out_packets = []
+for i,v in enumerate(out_packets):
+        if i == 0:
+                new_out_packets.append((v-v))
+        if i > 0:
+                new_out_packets.append((v) - (out_packets[(i - 1)]))
+
+print new_in_octets
+print new_in_packets
+print new_out_octets
+print new_out_packets
+
+# BUILD GRAPH1 (INPUT/OUTPUT OCTETS - INT Fa4) AND GRAPH2 (INPUT/OUTPUT PACKETS - INT Fa4)
+
+BuildGraph('1')
+BuildGraph('2')
